b7a348628bb4d6f6b612c372568ce0ee2d212249
GrimaceOfDespair <GrimaceOfDespair@hotmail.com>
2012-10-11 03:08:09 +0200

    Setup project files

---
+++ MessengerAppender/LiveAppender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net.Appender;
+using log4net.Core;
+
+namespace MessengerAppender
+{
+  public class LiveAppender : AppenderSkeleton
+  {
+    public LiveAppender()
+    {
+    }
+
+    protected override void Append(LoggingEvent loggingEvent)
+    {
+    }
+  }
+}
